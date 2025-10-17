81f09412f852e63bcfc65fb393e4769efe26d72b
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-10-17 20:02:42 +0200

    feat: using concept of Generic Repository

---
+++ GymMangmentDAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using GymMangmentDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangmentDAL.Repositories.Interfaces
+{
+    internal interface IGenericRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        TEntity? GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        int Add(TEntity entity);
+        int Update(TEntity entity);
+        int Delete( TEntity entity);
+
+
+    }
+}
